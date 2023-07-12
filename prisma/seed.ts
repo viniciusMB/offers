627734a8a5101b3709590d75c61145c103328534
kali <kali@kali.kali>
2023-07-12 18:45:49 -0300

    fix: seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -7,84 +7,91 @@ async function seed() {
     // Create currencies
     const currency1 = await prisma.currency.create({
       data: {
-        name: 'BTC',
+        name: 'USD',
       },
     });
 
     const currency2 = await prisma.currency.create({
       data: {
-        name: 'LSK',
+        name: 'EUR',
       },
     });
 
-    // Create users
-    await prisma.user.create({
+    const user1 = await prisma.user.create({
       data: {
         name: 'John Doe',
         email: 'john@example.com',
-        wallets: {
-          create: [
-            {
-              quantity: 100.0,
-              currency: {
-                connect: { id: currency1.id },
-              },
-            },
-            {
-              quantity: 200.0,
-              currency: {
-                connect: { id: currency2.id },
-              },
-            },
-          ],
+      },
+    });
+
+    const user2 = await prisma.user.create({
+      data: {
+        name: 'Jane Smith',
+        email: 'jane@example.com',
+      },
+    });
+
+    // Create wallets
+    const wallet1 = await prisma.wallet.create({
+      data: {
+        quantity: 100.0,
+        currency: {
+          connect: { id: currency1.id },
         },
-        offers: {
-          create: [
-            {
-              price: 10,
-              quantity: 5,
-              currency: {
-                connect: { id: currency1.id },
-              },
-            },
-            {
-              price: 20,
-              quantity: 10,
-              currency: {
-                connect: { id: currency2.id },
-              },
-            },
-          ],
+        user: {
+          connect: { id: user1.id },
         },
       },
     });
 
-    await prisma.user.create({
+    const wallet2 = await prisma.wallet.create({
       data: {
-        name: 'Jane Smith',
-        email: 'jane@example.com',
-        wallets: {
-          create: [
-            {
-              quantity: 500.0,
-              currency: {
-                connect: { id: currency1.id },
-              },
-            },
-          ],
+        quantity: 200.0,
+        currency: {
+          connect: { id: currency2.id },
         },
-        offers: {
-          create: [
-            {
-              price: 15,
-              quantity: 8,
-              currency: {
-                connect: { id: currency1.id },
-              },
-            },
-          ],
+        user: {
+          connect: { id: user1.id },
         },
       },
+    });
+
+    const wallet3 = await prisma.wallet.create({
+      data: {
+        quantity: 500.0,
+        currency: {
+          connect: { id: currency1.id },
+        },
+        user: {
+          connect: { id: user2.id },
+        },
+      },
+    });
+
+    await prisma.offer.createMany({
+      data: [
+        {
+          price: 10,
+          quantity: 5.0,
+          currencyId: currency1.id,
+          walletId: wallet1.id,
+          userId: user1.id,
+        },
+        {
+          price: 20,
+          quantity: 10.0,
+          currencyId: currency2.id,
+          walletId: wallet2.id,
+          userId: user1.id,
+        },
+        {
+          price: 12,
+          quantity: 4.0,
+          currencyId: currency1.id,
+          walletId: wallet3.id,
+          userId: user2.id,
+        },
+      ],
     });
 
     console.log('Seed data created successfully.');
