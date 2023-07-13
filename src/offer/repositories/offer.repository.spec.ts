259876802d58369255f4d42c4ae03a2fb4643da8
kali <kali@kali.kali>
2023-07-13 07:37:22 -0300

    chore: remove unused files

--- src/offer/repositories/offer.repository.spec.ts
+++ src/offer/repositories/offer.repository.spec.ts
@@ -2,17 +2,17 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { OfferRepository } from './offer.repository';
 
 describe('OfferRepository', () => {
-  let service: OfferRepository;
+  let repository: OfferRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [OfferRepository],
     }).compile();
 
-    service = module.get<OfferRepository>(OfferRepository);
+    repository = module.get<OfferRepository>(OfferRepository);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(repository).toBeDefined();
   });
 });
